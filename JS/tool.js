78e1adba8e724dc33b2497619a2c6a5c3a2a680e
theezejc <lizjurassicpark@hotmail.com>
2019-05-04 14:13:52 -0600

    Update3

--- JS/tool.js
+++ JS/tool.js
@@ -1,7 +1,23 @@
-<table>
-    <tr>
-        <th>
-            
-        </th>
-    </tr>
-</table>
+var firebaseConfig = {
+    apiKey: "AIzaSyBehvndrllXvVKQQpFp3hkMfK_T68IFdYw",
+    authDomain: "georockdb.firebaseapp.com",
+    databaseURL: "https://georockdb.firebaseio.com",
+    projectId: "georockdb",
+    storageBucket: "georockdb.appspot.com",
+    messagingSenderId: "811717337078",
+    appId: "1:811717337078:web:443474dd1553895d"
+};
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var ref = database.ref();
+
+ref.on('value', gotData, errData);
+
+function gotData(data) {
+  console.log(data.val());
+  var georockdb = data.val();
+}
+function errData(err){
+  console.log("Error!")
+  console.log(err)
+}
