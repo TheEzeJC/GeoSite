1782bdbee5728b4d0e22f33708335e48ecd73d3a
theezejc <lizjurassicpark@hotmail.com>
2019-05-04 20:14:58 -0600

    Searchbar

--- JS/tool.js
+++ JS/tool.js
@@ -3,11 +3,32 @@ $(document).ready(function(){
         var rock_data= '';
         $.each(data, function(key, value){
             rock_data += '<tr>';
-            rock_data += '<td>'+value.rockName+'<td>';
-            rock_data += '<td>'+value.rockType+'<td>';
-            rock_data += '<td>'+value.rockColor+'<td>';
+            rock_data += '<td>'+value.rockName+'</td>';
+            rock_data += '<td>'+value.rockType+'</td>';
+            rock_data += '<td>'+value.rockColor+'</td>';
+            rock_data += '<td>'+value.rockDesc+'</td>';
             rock_data += '</tr>';
         });
         $('#rockt').append(rock_data);
     });
 });
+
+function searchBar() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("userInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("rockt");
+    tr = table.getElementsByTagName("tr");
+  
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
+    }
+  }
