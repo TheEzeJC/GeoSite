aa9fe924397adc1bee2c0d7d1bec4360c163131b
theezejc <lizjurassicpark@hotmail.com>
2019-05-04 19:30:02 -0600

    table2

--- JS/tool.js
+++ JS/tool.js
@@ -1,2 +1,13 @@
-function CreateTableFromJSON(){
-}
+$(document).ready(function(){
+    $.getJSON("/GeoSite/JSON/data.json", function(data){
+        var rock_data= '';
+        $.each(data, function(key, value){
+            rock_data += '<tr>';
+            rock_data += '<td>'+value.rockName+'<td>';
+            rock_data += '<td>'+value.rockType+'<td>';
+            rock_data += '<td>'+value.rockColor+'<td>';
+            rock_data += '</tr>';
+        });
+        $('#rockt').append(rock_data);
+    });
+});
